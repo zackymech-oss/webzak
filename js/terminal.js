d2cd6ee560846c14ec6ee4872eb90330ea6214ab
zackymech-oss <zackymech@gmail.com>
2026-09-01 00:41:27 +0700

    Update terminal.js

--- js/terminal.js
+++ js/terminal.js
@@ -70,9 +70,8 @@ function handleLogin(input) {
     if (USERS[user] && USERS[user] === pass) {
         currentUser = user;
         
-        // --- TAMBAH BARIS INI BIAR INGAT SESSION LOGIN ---
-        localStorage.setItem('auth_session', 'true');
-        // ------------------------------------------------
+        // Simpan ke sessionStorage (otomatis ilang pas tab ditutup)
+        sessionStorage.setItem('auth_session', 'true');
 
         printOutput(`<span class="success">Access granted! Selamat datang di bangzaki.hub.</span>`);
         printOutput(`Ketik '<span class="system">help</span>' untuk panduan perintah.\n`);
